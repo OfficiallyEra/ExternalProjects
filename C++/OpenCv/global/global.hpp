2e01e0ba6674f583090add1ae4fe6223635fc686
OfficiallyEra <OffliciallyEra@users.noreply.github.com>
2024-08-28 18:54:06 +0530

    Updated : 28-08-2024

---
+++ C++/OpenCv/global/global.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+/*Section [C++ Compiler associates]*/
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+/*Section [OpenCv]*/
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
